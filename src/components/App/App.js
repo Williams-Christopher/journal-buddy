715aae30610c7829bf70cc1ac23271ceade46026
Christopher Williams <cw@chriswillia.ms>
2019-11-10 14:26:34 -0600

    Adds a basic footer to the app.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Navigation from '../Navigation/Navigation';
+import Footer from '../Footer/Footer';
 import About from '../../pages/About/About';
 import JournalEntryList from '../../pages/JournalEntryList/JournalEntryList';
 import FourOhFour from '../../pages/FourOhFour/FourOhFour';
@@ -18,7 +19,7 @@ class App extends React.Component {
             <Route path='/List' render={componentProps => <JournalEntryList entries={this.props.entries} />} />
             <Route component={FourOhFour} />
           </Switch>
-        <h1>Placeholder body text</h1>
+        <Footer />
       </>
     );
   };
