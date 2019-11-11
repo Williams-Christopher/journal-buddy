fa53480211a82b972dae7deca787d3611e8848b7
Christopher Williams <cw@chriswillia.ms>
2019-11-10 20:37:45 -0600

    Adds the registration page.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,7 @@ import About from '../../pages/About/About';
 import JournalEntryList from '../../pages/JournalEntryList/JournalEntryList';
 import CreateEntry from '../../pages/CreateEntry/CreateEntry';
 import ViewEntry from '../../pages/ViewEntry/ViewEntry';
+import Register from '../../pages/Register/Register';
 import FourOhFour from '../../pages/FourOhFour/FourOhFour';
 
 
@@ -22,6 +23,7 @@ class App extends React.Component {
             <Route path='/List' render={componentProps => <JournalEntryList entries={this.props.entries} />} />
             <Route path='/Create' component={CreateEntry} />
             <Route path='/View/:entry_id' render={({ match }, componentProps) => <ViewEntry match={match} entries={this.props.entries} />} />
+            <Route path='/Register' component={Register} />
             <Route component={FourOhFour} />
           </Switch>
         <Footer />
