457d76ac559fd6d78afa36be52784627c13da99f
Christopher Williams <cw@chriswillia.ms>
2019-11-13 20:02:01 -0600

    Reimplements the sticky footer with flexbox and replaces some About text with some notes.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -13,14 +13,7 @@ class About extends React.Component {
                     </h2>
                     <img className='about_image' src="https://picsum.photos/300/300" alt="Journal Buddy Usage Screenshot" />
                     <article className='about_explainer_text'>
-                        Journaling has many benefits. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Tincidunt vitae semper quis lectus. Suspendisse
-                        ultrices gravida dictum fusce ut placerat orci nulla. Rutrum tellus pellentesque eu tincidunt tortor
-                        aliquam. Faucibus purus in massa tempor nec. Amet dictum sit amet justo donec enim diam vulputate ut.
-                        Habitasse platea dictumst vestibulum rhoncus est. Tellus in hac habitasse platea dictumst vestibulum
-                        rhoncus est pellentesque. Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque.
-                        Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Amet consectetur adipiscing
-                        elit duis tristique sollicitudin nibh.
+                    Journaling has many benefits, among them increased mindfulness, increasing creativity, boosting memory, and improving self-discipline. Journaling can help reduce anxiety, relieive depression, build self-confidence, and help manage stress. With all these benefits to journaling, it's not surprising that the most succesful people in the world journal. In a few minutes a day these benefits can be yours.
                     </article>
                 </section>
                 <section className='about_explainer'>
@@ -28,14 +21,7 @@ class About extends React.Component {
                         Metrics and Goals and Things
                     </h2>
                     <article className='about_explainer_text'>
-                        Journaling has many benefits. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Tincidunt vitae semper quis lectus. Suspendisse
-                        ultrices gravida dictum fusce ut placerat orci nulla. Rutrum tellus pellentesque eu tincidunt tortor
-                        aliquam. Faucibus purus in massa tempor nec. Amet dictum sit amet justo donec enim diam vulputate ut.
-                        Habitasse platea dictumst vestibulum rhoncus est. Tellus in hac habitasse platea dictumst vestibulum
-                        rhoncus est pellentesque. Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque.
-                        Sollicitudin nibh sit amet commodo nulla facilisi nullam vehicula ipsum. Amet consectetur adipiscing
-                        elit duis tristique sollicitudin nibh.
+                        Journal Buddy is here to help. Use Journal Buddy to make your entries: recount your challenges, jot down your ideas, note the things your're thankful for in Journal Buddy's easy to use, distraction free interface. Journaling is helpful only if it's done regularly. Use Journal Buddy's metrics to find out what days of the week you are most or least likely to journal. See counts of your entries by month or days of the week. Other stuff. Yadda yadda lorem ipsum an dall that other fun stuff that makes life worth living in this amazing, advanced, and craxy day and age.
                     </article>
                     <img className='about_image' src="https://picsum.photos/300/300" alt="Journal Buddy Usage Screenshot" />
                     <Link className='common_link' to='Register'>Sign up!</Link>
