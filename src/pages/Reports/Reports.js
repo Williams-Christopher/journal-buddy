f8173b1a013e1208aabb2cc8bae93277db873497
Christopher Williams <cw@chriswillia.ms>
2019-12-09 23:54:45 -0600

    Implements metrics for MVP.

--- src/pages/Reports/Reports.js
+++ src/pages/Reports/Reports.js
@@ -1,21 +1,120 @@
 import React from 'react';
 
+import { Bar } from 'react-chartjs-2';
+
+import ApiServices from '../../services/api-services';
+
 import './Reports.css';
 
 class Reports extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.graphDataKeys = ['total_by_day', 'total_by_feeling', 'total_by_month'];
+        this.chartOptions = {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        autoSkip: true,
+                    },
+                }],
+            },
+        };
+        this.state = {
+            error: null,
+            requestComplete: false,
+            data: {},
+        };
+    };
+
+    componentDidMount() {
+        this.setState({
+            error: null,
+        });
+
+        ApiServices.getMetrics()
+            .then(result =>
+                this.setState({
+                    data: { ...result },
+                    requestComplete: true,
+                }, () => this.prepareDataSets())
+            )
+            .catch(error => this.setState({ error: error.error }));
+    };
+
+    prepareDataSets = () => {
+        for (const key of this.graphDataKeys) {
+            this.prepareDataSet(key);
+        };
+    };
+
+    prepareDataSet = (key) => {
+        const chartData = Object.values(this.state.data[key]);
+        let chartLabels;
+        if (key === 'total_by_feeling') {
+            chartLabels = ['Very Down', 'Down', 'Normal/Even', 'Happy', 'Very Happy'];
+        } else {
+            chartLabels = Object.keys(this.state.data[key]);
+        };
+
+        const dataObject = {
+            labels: chartLabels,
+            datasets: [{
+                label: this.createTitleCaseHeadingFromKey(key),
+                data: chartData,
+            }],
+        };
+
+        this.setState({
+            [key]: dataObject,
+        });
+    };
+
+    createBarGraph = (key) => {
         return (
-            <section className='journal_reports'>
-                <h2>
-                    Journal Reports
+            <div className='metrics-graph'>
+                <Bar data={this.state[key]} options={this.chartOptions} />
+            </div>
+        )
+    };
+
+    createTitleCaseHeadingFromKey = (key) => {
+        const words = key.split('_').map(word => word[0].toUpperCase() + word.slice(1, word.length));
+        return words.join(' ');
+    };
+
+    render() {
+        if (this.state.requestComplete) {
+            return (
+                <section className='journal_reports'>
+                    <h2>
+                        Journal Reports
                 </h2>
-                <p>#Small chart with a count of journal entries by month#</p>
-                <p>#Small charqt with a count of journal entries by day of week#</p>
-                <p>#Small chart with a count of journal entries by feeling#</p>
-                <p>#Can we chart the trend feeling metric over the last 30 or 60 days?#</p>
-                <p><br />Return to previous page</p>
-            </section>
-        );
+
+                    <section className='metrics-containter'>
+                        <section className='metrics-tiles-container'>
+                            <div className='metrics-tile'>
+                                <p>Total Entries</p>
+                                <p>{this.state.data.total_entries}</p>
+                            </div>
+                            <div className='metrics-tile'>
+                                <p>Private Entries</p>
+                                <p>{this.state.data.private_entries}</p>
+                            </div>
+                            <div className='metrics-tile'>
+                                <p>Public Entries</p>
+                                <p>{this.state.data.public_entries}</p>
+                            </div>
+                        </section>
+                        <section className='metrics-graphs-container'>
+                            {this.graphDataKeys.map(key => this.createBarGraph(key))}
+                        </section>
+                    </section>
+                    <p><br />Return to previous page</p>
+                </section>
+            )
+        } else {
+            return <p>Preparing...</p>
+        };
     };
 };
 
