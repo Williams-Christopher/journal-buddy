afbcc463fcd3d2cab3b307e3738780a2dd8b0616
Christopher Williams <cw@chriswillia.ms>
2019-12-20 00:10:53 -0600

    Updates test for create entry page.

--- src/pages/CreateEntry/CreateEntry.test.js
+++ src/pages/CreateEntry/CreateEntry.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
 
 import CreateEntry from './CreateEntry';
+import TestHelpers from '../../helpers/TestHelpers';
 
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<CreateEntry />, div);
+    ReactDOM.render(
+        <BrowserRouter>
+            <Router history={TestHelpers.dummyHistory}>
+                <CreateEntry entries={TestHelpers.entries} />
+            </Router>
+        </BrowserRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
